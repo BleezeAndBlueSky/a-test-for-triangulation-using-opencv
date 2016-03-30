31ba9fa3a367a4f3fba3cfab608c5f292c7df5a1
TauBai <traumland@hotmail.com>
2016-03-31 07:40:47 +0800

    Create harrisdetector_demo.cpp

---
+++ feature-based/harrisdetector_demo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "harrisdetector.h"
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+        return -1;
+    cv::Mat image = cv::imread(argv[1]);
+    cv::cvtColor(image,image,CV_RGB2GRAY);
+    
+    HarrisDetector harris;
+    // Compute Harris values
+    harris.detect(image);
+    // Detect Harris corners
+    std::vector<cv::Point> pts;
+    harris.getCorners(pts,0.02);
+    // Draw Harris corners
+    harris.drawOnImage(image,pts);
+    cv::namedWindow("harriscorner",1);
+    cv::imshow("harriscorner", image);
+
+
+    cv::waitKey(0);
+    return 0;
+
+
+}

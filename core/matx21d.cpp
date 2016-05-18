d7c88e67895692dffb9d8878de2ecc3788820bb6
TauBai <traumland@hotmail.com>
2016-05-19 07:30:22 +0800

    Update and rename matx21d to matx21d.cpp

--- core/matx21d.cpp
+++ core/matx21d.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -8,7 +9,7 @@ int main()
     cv::Matx22d m2( 1,1,
                     1,1);
     cv::Matx21d m3 = m2* m1;
-    m3 (2,1);   // 第二行第一列取值
+    m3 (2,1);   // 第二行第一列取值 注:由于过去很久时间,这段代码没有再试验,在其他的程序中 m(i,j),i,j是从0开始的,特此注明
     std::cout << m3 <<std::endl;
 
 }

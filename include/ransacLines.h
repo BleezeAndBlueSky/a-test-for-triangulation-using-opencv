09ad6e4bbeb2aec50da7646823ffbe415bb03e9c
TauBai <traumland@hotmail.com>
2016-05-20 12:36:24 +0800

    Update ransacLines.h

--- include/ransacLines.h
+++ include/ransacLines.h
@@ -9,6 +9,10 @@
  *                                 -----------------2016. 5.18   00:40
  *  搞错了 ,  应该是40次左右就能达到99%,  还是程序本身的问题,  难道是rand()的伪随机?
  *                                  ----------------2016. 5.19   16:00
+ *使用了c++11 中的随机函数, 对于非指定边数的不规则形状(内凹, 很多杂乱的边缘)的点效果较差, 因为这个函数本身就只是用来识别直线特征的
+ *   40次可以达到效果, 1000次基本符合我的预期
+ *                                  ----------------2016. 5.20    12.33
+ *--------------------------------------------------------------------------------------
  * 注意:
  *最后一步没有用确定出inlier的点进行重新拟合,有的算法书会用到这一点. 如果需要,加个fitline函数就行了,或者自己去写,也很简单
  */
@@ -36,13 +40,16 @@ void ransacLines(std::vector<cv::Point>& input,std::vector<cv::Vec4d>& lines,
         cv::Point jmax;
         cv::Vec4d line;
         size_t t1 , t2;
+        
+        std::random_device rd;     // only used once to initialise (seed) engine
+        std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
+        std::uniform_int_distribution<int> uni(0,input.size()-1); // guaranteed unbiased // 概率相同
+
         unsigned int it = itmax;
         while(--it){
-            srand((unsigned) time(NULL));
-            t1 = rand() % input.size();
-            srand((unsigned) time(NULL));
-            t2 = rand() % input.size();
-            t2 = (t1 == t2 ? rand() % input.size() : t2 );
+            t1 = uni(rng);
+            t2 = uni(rng);
+            t2 = (t1 == t2 ? uni(rng): t2);
             unsigned int M = 0;
             cv::Point i = input[t1];
             cv::Point j = input[t2];
